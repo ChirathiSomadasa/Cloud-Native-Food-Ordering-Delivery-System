7410614be84e7b9aa2bb94793799d2822dff0ec8
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-03-24 19:17:58 +0530

    restaurant-management-service Connect to the database

---
+++ restaurant_management_service/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/dbConfig');
+const { PORT } = require('./config/envConfig');
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+
+
+// Connect to the database and start the server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch((err) => {
+  console.error('Failed to start the server:', err.message);
+});
