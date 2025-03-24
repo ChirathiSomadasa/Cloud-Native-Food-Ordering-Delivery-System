43da99d16bbdf6537af440cdc7e6fc54f65fab16
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-03-24 17:44:07 +0530

    Implement authentication mechanisms

--- user_authentication_service/server.js
+++ user_authentication_service/server.js
@@ -3,16 +3,20 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/dbConfig');
 const { PORT } = require('./config/envConfig');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Connect to the database and start the server
 connectDB().then(() => {
@@ -20,5 +24,5 @@ connectDB().then(() => {
     console.log(`Server is running on port ${PORT}`);
   });
 }).catch((err) => {
-  console.error("Failed to start the server:", err.message);
+  console.error('Failed to start the server:', err.message);
 });
