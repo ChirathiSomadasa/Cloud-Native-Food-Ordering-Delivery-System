bca54ff37670f53aa6c063f3e2758e728512c629
Udeshika Balasooriya <159142506+UdeshikaP@users.noreply.github.com>
2025-03-26 15:40:26 +0530

    check getorder

--- order_management_service/routes/orderRoutes.js
+++ order_management_service/routes/orderRoutes.js
@@ -4,8 +4,8 @@ const { verifyToken, verifyRole } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/add', verifyToken, verifyRole(['customer']), placeOrder);
-// router.get('/:id', verifyToken, verifyRole(['customer', 'restaurantAdmin']), getOrder);
+router.post('/add', verifyToken, verifyRole(['customer']), placeOrder);//checked
+router.get('/:id', verifyToken, verifyRole(['customer', 'restaurantAdmin']), getOrder);//checked
 // router.get('/', verifyToken, verifyRole(['restaurantAdmin']), getOrdersForRestaurant);
 // router.put('/:id/status', verifyToken, verifyRole(['restaurantAdmin']), updateOrderStatus);
 // router.delete('/:id', verifyToken, verifyRole(['customer']), cancelOrder);
