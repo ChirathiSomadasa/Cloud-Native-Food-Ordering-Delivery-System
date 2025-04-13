c04fc9b43c2091c842ed78e2bc91d7c8c74d739d
Udeshika Balasooriya <159142506+UdeshikaP@users.noreply.github.com>
2025-04-13 17:40:02 +0530

    update order model

--- order_management_service/models/Order.js
+++ order_management_service/models/Order.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 const objectId = Schema.ObjectId;
-
+ 
 const orderSchema = new Schema(
     {
         id: objectId,
         customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
+        //restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
         itemId: [{type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: true }],
         quantity: { type: Number, required: true, min: 1 },
         totalPrice: { type: Number, required: true },
