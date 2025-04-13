95ad96e1069ac739ffef07a52cf6509d0f2f0c18
Udeshika Balasooriya <159142506+UdeshikaP@users.noreply.github.com>
2025-04-13 17:08:25 +0530

    update cart routes

--- order_management_service/routes/cartRoutes.js
+++ order_management_service/routes/cartRoutes.js
@@ -4,8 +4,9 @@ const cartController = require("../controllers/cartController");
 const { verifyToken } = require("../middleware/authMiddleware");
 
 router.post("/add", verifyToken, cartController.addToCart);
-router.get("/", verifyToken, cartController.getCart);
-router.delete("/remove/:itemId", verifyToken, cartController.removeFromCart);
+router.get("/", verifyToken, cartController.getUserCart);
+router.delete("/remove/:id", verifyToken, cartController.removeFromCart);
+router.put("/update",verifyToken, cartController.updateQuantity);
 
 module.exports = router;
  
