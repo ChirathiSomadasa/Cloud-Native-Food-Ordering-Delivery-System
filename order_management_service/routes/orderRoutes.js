625b404d7e2328efccc3f624a16d269aff2a0c36
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-04-27 18:16:54 +0530

    update

--- order_management_service/routes/orderRoutes.js
+++ order_management_service/routes/orderRoutes.js
@@ -1,20 +1,35 @@
 const express = require('express');
-const { placeOrder, getOrder, getOrdersForRestaurant,deleteOrderForRestaurant,
-     updateOrderStatus, updateOrder,cancelOrder,getOrdersForCustomer,getRestaurantOrders,getNewOrderCount } = require('../controllers/orderController');
+const { placeOrder, 
+     getOrder, 
+     getOrdersForRestaurant,
+     deleteOrderForRestaurant,
+     updateOrderStatus, 
+     updateOrder,
+     cancelOrder,
+     getOrdersForCustomer,
+     getRestaurantOrders,
+     getNewOrderCount } = require('../controllers/orderController');
 const { verifyToken, verifyRole } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Customer Order Routes
+//create new order
 router.post('/add',verifyToken,verifyRole(['customer']), placeOrder);//checked
 //router.get('/:id', verifyToken, verifyRole(['customer', 'restaurantAdmin']), getOrder);//checked
+//delete order by customer
 router.delete('/:id', verifyToken, verifyRole(['customer']), cancelOrder);//checked
-router.get('/:customerId',  getOrdersForCustomer); // Get all orders for a customer
+//get orders by specific customer
+router.get('/:customerId',  getOrdersForCustomer); 
+//update order by customer
 router.put("/:id/update", verifyToken,verifyRole(['customer']), updateOrder);
 
 // Restaurant Admin Routes
 //router.get('/restaurant/:restaurantId', getOrdersForRestaurant);
+//update order status by restaurant admin
 router.put('/update-status/:orderId', verifyToken, verifyRole(['restaurantAdmin']), updateOrderStatus);//checked
+//get orders by specific restaurant admin
 router.get('/restaurant/:restaurantId', verifyToken, verifyRole(['restaurantAdmin']), getRestaurantOrders);
+//delete order by restaurant admin
 router.delete('/delete/:orderId', verifyToken, verifyRole(['restaurantAdmin']), deleteOrderForRestaurant);//checked
 router.get('/new-count', verifyToken, verifyRole(['restaurantAdmin']),getNewOrderCount);
 
